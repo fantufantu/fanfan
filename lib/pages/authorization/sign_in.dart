a8e78c7cd1be8e2901895af5336d1d2b09e15bac
murukal <tutu@fantufantu.com>
2023-04-04 00:33:44 +0800

    feat: 登录页的状态管理和样式设计

--- lib/pages/authorization/sign_in.dart
+++ lib/pages/authorization/sign_in.dart
@@ -1,57 +1,113 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: Container(
-          padding: const EdgeInsets.only(left: 20),
-          child: IconButton(
-            onPressed: () => {context.go('/')},
-            icon: const Icon(Icons.arrow_back),
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
-          ),
-        ),
-      ),
-      body: const _SignIn(),
-    );
+    return const _SignInForm();
   }
 }
 
-class _SignIn extends StatefulWidget {
-  const _SignIn();
+class _SignInForm extends StatefulWidget {
+  const _SignInForm();
 
   @override
-  _SignInState createState() => _SignInState();
+  _SignInFormState createState() => _SignInFormState();
 }
 
-class _SignInState extends State<_SignIn> {
+class _SignInFormState extends State<_SignInForm> {
   /// 用户名、邮箱
-  String keyword = '';
+  String _keyword = '';
 
   /// 密码
-  String password = '';
+  String _password = '';
+
+  /// 记住我
+  bool _isRememberMe = false;
 
   @override
   Widget build(BuildContext context) {
     return Form(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          TextFormField(
-            decoration: InputDecoration(
-              label: Text("请输入用户名"),
+          Container(
+            margin: const EdgeInsets.only(
+              top: 28,
+            ),
+            child: const Text(
+              'Login to your',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(
+              top: 12,
+              bottom: 40,
+            ),
+            child: const Text(
+              'Account',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
           TextFormField(
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
+              label: Text("请输入用户名"),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(12),
+                ),
+                borderSide: BorderSide(
+                  style: BorderStyle.none,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(12),
+                ),
+                borderSide: BorderSide(
+                  style: BorderStyle.none,
+                ),
+              ),
+              filled: true,
+              fillColor: Colors.black12,
+              focusColor: Colors.black45,
+              hoverColor: Colors.black45,
+              floatingLabelStyle: TextStyle(
+                color: Colors.transparent,
+              ),
+            ),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
               label: Text("请输入密码"),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Checkbox(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(4),
+                    ),
+                  ),
+                  value: _isRememberMe,
+                  onChanged: (changed) => setState(() {
+                    _isRememberMe = changed == true;
+                  }),
+                ),
+                const Text('记住我')
+              ],
             ),
           ),
         ],
